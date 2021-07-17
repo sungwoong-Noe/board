ffa66818a75c68c72f1822cb8a771c6cc446d0ed
Noe <asdz453@gmail.com>
2021-07-18 01:35:35 +0900

    데이터 베이스 변경, 게시글 작성 구현

--- src/main/java/simpleboard/board/controller/BoardController.java
+++ src/main/java/simpleboard/board/controller/BoardController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import simpleboard.board.model.Board;
 import simpleboard.board.repository.BoardRepository;
+
 
 import java.util.List;
 
@@ -27,4 +30,18 @@ public class BoardController {
         return "board/list";
     }
 
+    @GetMapping("/form")
+    public String form(Model model){
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){  //보드를 받아오는 컨트롤러 작성
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
+
 }
+
